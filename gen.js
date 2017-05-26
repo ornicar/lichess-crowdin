09ae695818b0c935ac48090f6ce7b8eed6671da8
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-05-26 22:48:51 +0200

    more progress on XML generation and tweaks

--- gen.js
+++ gen.js
@@ -1,5 +1,6 @@
 const fs = require('fs-extra');
 const xmlBuilder = require('xmlbuilder');
+const tweaks = require('./tweaks');
 
 const allLangs = 'en,fr,ru,de,tr,sr,lv,bs,da,es,ro,it,fi,uk,pt,pl,nl,vi,sv,cs,sk,hu,ca,sl,az,nn,eo,tp,el,fp,lt,nb,et,hy,af,hi,ar,zh,gl,hr,mk,id,ja,bg,th,fa,he,mr,mn,cy,gd,ga,sq,be,ka,sw,ps,is,kk,io,gu,fo,eu,bn,id,la,jv,ky,pi,as,le,ta,sa,ml,kn,ko,mg,kb,zu,ur,yo,tl,fy,jb,tg,cv,ia,tc'.split(',').filter(l => l != 'en');
 
@@ -41,152 +42,25 @@ function findByKey(haystack, needle) {
   return (haystack.find(c => c[0] === needle) || [])[1];
 }
 
-const tweaks = {
-  gamesBeingPlayedRightNow: {
-    rename: 'currentGames'
-  },
-  aiNameLevelAiLevel: {
-    type: 'string'
-  },
-  nbConnectedPlayers: {
-    rename: 'nbPlayers'
-  },
-  viewAllNbGames: {
-    rename: 'nbGames',
-    one: 'One game'
-  },
-  viewNbCheckmates: {
-    rename: 'nbCheckmates',
-    one: 'One checkmate'
-  },
-  nbBookmarks: {
-    one: 'One bookmark'
-  },
-  xPostedInForumY: {
-    type: 'string'
-  },
-  nbDays: {
-    one: 'One day'
-  },
-  nbHours: {
-    one: 'One hour'
-  },
-  nbGamesWithYou: {
-    one: 'One game with you'
-  },
-  nbWins: {
-    one: 'One win'
-  },
-  nbLosses: {
-    one: 'One loss'
-  },
-  nbDraws: {
-    one: 'One draw'
-  },
-  nbMembers: {
-    one: 'One member'
-  },
-  xJoinedTeamY: {
-    type: 'string'
-  },
-  xCreatedTeamY: {
-    type: 'string'
-  },
-  nbImportedGames: {
-    one: 'One imported game'
-  },
-  colorPlaysCheckmateInOne: {
-    type: 'string'
-  },
-  xStartedFollowingY: {
-    type: 'string'
-  },
-  nbFollowers: {
-    one: 'One follower'
-  },
-  lastSeenActive: {
-    type: 'string'
-  },
-  lessThanNbMinutes: {
-    one: 'Less than a minute'
-  },
-  reportXToModerators: {
-    type: 'string'
-  },
-  yourPuzzleRatingX: {
-    type: 'string'
-  },
-  puzzleId: {
-    type: 'string'
-  },
-  ratingX: {
-    type: 'string'
-  },
-  playedXTimes: {
-    one: 'Played one time'
-  },
-  fromGameLink: {
-    type: 'string'
-  },
-  nbGamesInPlay: {
-    one: 'One game in play'
-  },
-  by: {
-    type: 'string'
-  },
-  claimDrawOnThreefoldRepetitionAutomatically: {
-    dropMarkers: true, // "%sthreefold repetition%s" -> "threefold repetition"
-  },
-  xLeftANoteOnY: {
-    type: 'string'
-  },
-  xCompetesInY: {
-    type: 'string'
-  },
-  xAskedY: {
-    type: 'string'
-  },
-  xAnsweredY: {
-    type: 'string'
-  },
-  xCommentedY: {
-    type: 'string'
-  },
-  ifRatingIsPlusMinusX: {
-    type: 'string'
-  },
-  nbForumPosts: {
-    one: 'One forum post'
-  },
-  tpTimeSpentPlaying: {
-    type: 'string'
-  },
-  tpTimeSpentOnTV: {
-    type: 'string'
-  },
-  weeklyPerfTypeRatingDistribution: {
-    type: 'string'
-  },
-  nbPerfTypePlayersThisWeek: {
-    one: 'One %s players this week.'
-  },
-  yourPerfTypeRatingisRating: {
-    type: 'string'
-  },
-  youAreBetterThanPercentOfPerfTypePlayers: {
-    // percent then plural
-  },
-  youDoNotHaveAnEstablishedPerfTypeRating: {
-    type: 'string'
-  },
-  weHaveSentYouAnEmailTo: {
-    type: 'string'
-  },
-  byRegisteringYouAgreeToBeBoundByOur: {
-    type: 'string'
-  },
-  youHaveAlreadyRegisteredTheEmail: {
-    type: 'string'
+function makeResource(key, source, context) {
+  const tweak = tweaks[key] || {};
+  const name = tweak.rename || key;
+  let resource, i, english;
+  return (doc) => {
+    if (!source.includes('%s') || tweak.type === 'string') {
+      i = 1;
+      english = source.replace(/%s/g, () => '%' + (i++) + '$s');
+      resource = doc.ele('string', { name: name }, english);
+    } else {
+      i = 2;
+      let english = source
+        .replace(/%s/, () => '%1$d')
+        .replace(/%s/g, () => '%' + (i++) + '$d');
+      resource = doc.ele('plurals', { name: name });
+      resource.ele('item', { quantity: 'one' }, tweak.one || english);
+      resource.ele('item', { quantity: 'other' }, tweak.other || english);
+    }
+    if (context) resource.commentBefore(context.replace('--', '-'));
   }
 };
 
@@ -200,17 +74,7 @@ function makeSourceXml() {
       encoding: 'UTF-8'
     }).ele('resources');
     enTrans.forEach(en => {
-      const key = en[0];
-      const source = en[1];
-      const context = findByKey(contexts, source);
-      let item;
-      if (source.includes('%s')) {
-        item = resources.ele('plurals', { name: key });
-        item.ele('item', { quantity: 'one' }, source);
-        item.ele('item', { quantity: 'other' }, source);
-      }
-      else item = resources.ele('string', { name: key }, source);
-      if (context) item.commentBefore(context.replace('--', '-'));
+      makeResource(en[0], en[1], findByKey(contexts, en[1]))(resources);
     });
     const str = '<?xml version="1.0" encoding="UTF-8"?>\n' + resources.end({
       pretty: true,
---
+++ tweaks.js
@@ -0,0 +1,148 @@
+module.exports = {
+  gamesBeingPlayedRightNow: {
+    rename: 'currentGames'
+  },
+  aiNameLevelAiLevel: {
+    type: 'string'
+  },
+  nbConnectedPlayers: {
+    rename: 'nbPlayers'
+  },
+  viewAllNbGames: {
+    rename: 'nbGames',
+    one: 'One game'
+  },
+  nbBookmarks: {
+    one: 'One bookmark'
+  },
+  xPostedInForumY: {
+    type: 'string'
+  },
+  nbDays: {
+    one: 'One day'
+  },
+  nbHours: {
+    one: 'One hour'
+  },
+  nbGamesWithYou: {
+    one: 'One game with you'
+  },
+  nbWins: {
+    one: 'One win'
+  },
+  nbLosses: {
+    one: 'One loss'
+  },
+  nbDraws: {
+    one: 'One draw'
+  },
+  nbMembers: {
+    one: 'One member'
+  },
+  xJoinedTeamY: {
+    type: 'string'
+  },
+  xCreatedTeamY: {
+    type: 'string'
+  },
+  nbImportedGames: {
+    one: 'One imported game'
+  },
+  colorPlaysCheckmateInOne: {
+    type: 'string'
+  },
+  xStartedFollowingY: {
+    type: 'string'
+  },
+  nbFollowers: {
+    one: 'One follower'
+  },
+  lastSeenActive: {
+    type: 'string'
+  },
+  lessThanNbMinutes: {
+    one: 'Less than a minute'
+  },
+  reportXToModerators: {
+    type: 'string'
+  },
+  yourPuzzleRatingX: {
+    type: 'string'
+  },
+  puzzleId: {
+    type: 'string'
+  },
+  ratingX: {
+    type: 'string'
+  },
+  playedXTimes: {
+    one: 'Played one time'
+  },
+  fromGameLink: {
+    type: 'string'
+  },
+  nbGamesInPlay: {
+    one: 'One game in play'
+  },
+  by: {
+    type: 'string'
+  },
+  claimDrawOnThreefoldRepetitionAutomatically: {
+    dropTranslations: true
+  },
+  xLeftANoteOnY: {
+    type: 'string'
+  },
+  xCompetesInY: {
+    type: 'string'
+  },
+  xAskedY: {
+    type: 'string'
+  },
+  xAnsweredY: {
+    type: 'string'
+  },
+  xCommentedY: {
+    type: 'string'
+  },
+  ifRatingIsPlusMinusX: {
+    type: 'string'
+  },
+  nbForumPosts: {
+    one: 'One forum post'
+  },
+  tpTimeSpentPlaying: {
+    type: 'string'
+  },
+  tpTimeSpentOnTV: {
+    type: 'string'
+  },
+  weeklyPerfTypeRatingDistribution: {
+    type: 'string'
+  },
+  nbPerfTypePlayersThisWeek: {
+    one: 'One %2$s players this week.',
+    other: '%1$d %2$s players this week.',
+    dropTranslations: true
+  },
+  yourPerfTypeRatingisRating: {
+    type: 'string'
+  },
+  youAreBetterThanPercentOfPerfTypePlayers: {
+    one: 'You are better than %1$s of one player.', // never gonna happen
+    other: 'You are better than %1$s of %2$d players.',
+    dropTranslations: true
+  },
+  youDoNotHaveAnEstablishedPerfTypeRating: {
+    type: 'string'
+  },
+  weHaveSentYouAnEmailTo: {
+    type: 'string'
+  },
+  byRegisteringYouAgreeToBeBoundByOur: {
+    type: 'string'
+  },
+  youHaveAlreadyRegisteredTheEmail: {
+    type: 'string'
+  }
+};
